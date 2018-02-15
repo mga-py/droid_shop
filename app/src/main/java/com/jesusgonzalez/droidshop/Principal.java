b90c4d3573b9d12b2c09347558a4e10631c97385
Baltasar Mga-py <baltasarrangel93@gmail.com>
2018-02-15 10:05:51 +0100

    Navigation Drawer Activity Modified - Modificación de la barra de menú -- Añadir Nav productos (OK) -- Añadir Nav pedidos (OK) -- Añadir Nav carrito (OK) -- Añadir Nav clientes (OK) -- Añadir Nav perfiles (OK) -- Añadir Nav logout (OK)

--- app/src/main/java/com/jesusgonzalez/droidshop/Principal.java
+++ app/src/main/java/com/jesusgonzalez/droidshop/Principal.java
@@ -80,17 +80,17 @@ public class Principal extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_products) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_orders) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_cart) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_customes) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_logout) {
 
         }
 
